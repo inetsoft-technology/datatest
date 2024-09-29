bbd03b24432a0631aa9741211ef0779879aee7d3
sallyma <sallyma@inetsoft.com>
2024-09-29 16:21:52 +0800

    add case

---
+++ tabular/src/test/groovy/inetsoft/test/tabular/cases/Cassandra_Spec.groovy
@@ -0,0 +1,54 @@
+package inetsoft.test.tabular.cases
+
+import inetsoft.test.modules.GlobalTest
+import spock.lang.IgnoreRest
+import spock.lang.Specification
+
+class Cassandra_Spec extends Specification{
+   static String caseName
+   static GlobalTest globalTest
+
+   def setupSpec() {
+      GlobalTest.initHome(this.class.getName())
+   }
+
+   /*
+    check cassandra sql query and database query
+    covered composite table and condition
+    */
+   def 'cassandraDB query and composite table'() {
+      caseName = specificationContext.currentIteration.name
+      globalTest = new GlobalTest(caseName)
+      def paras1 = new HashMap<String, String[]>()
+      paras1.put('var_date1', ['2014-08-03 00:00:00'] as String[])
+      paras1.put('var_date2', ['2015-05-03 00:00:00'] as String[])
+      globalTest.executeTest('1^2^__NULL__^Cassandra/WS_Condition&Parameter', paras1)
+      globalTest.executeTest('1^2^__NULL__^Cassandra/WS_Join&Concatenate', null)
+
+      expect:
+      globalTest.compareData()
+   }
+
+   /*
+    check cassandra used in vs and report
+    covered parameters and filters
+    */
+   def 'cassandra used in vs and report'() {
+      caseName = specificationContext.currentIteration.name
+      globalTest = new GlobalTest(caseName)
+      def paras1 = new HashMap<String, String[]>()
+      def paras2 = new HashMap<String, String[]>()
+      paras1.put('day1', ['2014-07-03'] as String[])
+      paras1.put('day2', ['2015-05-03'] as String[])
+      paras2.put('var_date1', ['2014-05-02 20:13:23'] as String[])
+      paras2.put('var_date2', ['2015-03-03 20:13:23'] as String[])
+      paras2.put('var_f_paid', ['143'] as String[])
+      globalTest.executeTest('1^128^__NULL__^Cassandra/VS_Parameters', ['Home','state_CA'] as String[], paras1)
+      globalTest.executeTest('1^128^__NULL__^Cassandra/VS_Relation&Filter', ['Home','EmbTab1','filter2'] as String[], null)
+      globalTest.executeTest('Cassandra/R_Parameters1', null)
+      globalTest.executeTest('Cassandra/R_Parameters2', paras2)
+
+      expect:
+      globalTest.compareImage()
+   }
+}
---
+++ tabular/src/test/groovy/inetsoft/test/tabular/cases/ElasticSearch_Spec.groovy
@@ -0,0 +1,44 @@
+package inetsoft.test.tabular.cases
+
+import inetsoft.test.modules.GlobalTest
+import spock.lang.IgnoreRest
+import spock.lang.Specification
+
+class ElasticSearch_Spec extends Specification{
+   static String caseName
+   static GlobalTest globalTest
+
+   def setupSpec() {
+      GlobalTest.initHome(this.class.getName())
+   }
+
+   /*
+    check elasticsearch query and database query
+    */
+   def 'elasticsearch query and database query'() {
+      caseName = specificationContext.currentIteration.name
+      globalTest = new GlobalTest(caseName)
+      globalTest.executeTest('1^2^__NULL__^ElasticSearch Rest/ElasticSearch Database Query', null)
+      globalTest.executeTest('1^2^__NULL__^ElasticSearch Rest/ElasticSearch Query', null)
+
+      expect:
+      globalTest.compareData()
+   }
+
+   /*
+    check elasticsearch used in vs and report
+    */
+   def 'elasticsearch used in vs and report'() {
+      caseName = specificationContext.currentIteration.name
+      globalTest = new GlobalTest(caseName)
+      def paras1 = new HashMap<String, String[]>()
+      def paras2 = new HashMap<String, String[]>()
+      paras1.put('2015-05-18 19', ['2015-05-18 19:00:00'] as String[])
+      paras2.put('date', ['2015-05-18 19:00:00'] as String[])
+      globalTest.executeTest('1^128^__NULL__^ElasticSearch Rest/ElasticSearch VS', ['Home','bk1', 'filter'] as String[], paras1)
+      globalTest.executeTest('ElasticSearch Rest/Elastic Report', paras2)
+
+      expect:
+      globalTest.compareImage()
+   }
+}
