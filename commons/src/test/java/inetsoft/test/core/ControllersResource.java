daed6800620f7d841c51b968e27fbf14783f9298
sarapeng <littlesand@126.com>
2025-09-22 16:16:07 +0800

    update

--- commons/src/test/java/inetsoft/test/core/ControllersResource.java
+++ commons/src/test/java/inetsoft/test/core/ControllersResource.java
@@ -97,8 +97,10 @@ public class ControllersResource extends MockMessageResource {
       selectionService = null;
       imageService = null;
       worksheetService = null;
-      staticConfigurationContext.close();
-      staticConfigurationContext = null;
+      if (staticConfigurationContext != null) {
+         staticConfigurationContext.close();
+         staticConfigurationContext = null;
+      }
    }
 
    private void createControllers() {
